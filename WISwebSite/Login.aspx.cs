94c399cc99cec3a91802b6d6b1948b9d82ac32c3
Junhee Kim <juki207@ess.ais.ac.nz>
2021-03-13 13:59:19 +1300

    Add project files.

---
+++ WISwebSite/Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WISwebSite
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            //get studentName that matches with ID
+            string result = string.Empty;
+            result = DataAccessLayer.getStudentName(txtUserID.Text);
+
+            if (result.Equals("0"))
+            {
+                Response.Write("<script>alert('Please input valid ID or Password');</script>");
+                Response.Redirect("Default.aspx");
+            }
+            else {
+                string firstName = result;
+                //Save Cookie
+                HttpCookie aCookie = new HttpCookie("aUser");
+                aCookie.Values["firstName"] = firstName;
+                aCookie.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Add(aCookie);
+            }
+
+            bool isValidId = DataAccessLayer.verifyLogin(txtUserID.Text, txtPassword.Text);
+            if (isValidId)
+            {
+                Response.Redirect("Home.aspx");
+            }
+            else {
+                Response.Write("<script>alert('Please input valid ID or Password');</script>");
+                Response.Redirect("Default.aspx");
+            }
+        }
+
+        
+    }
+}
